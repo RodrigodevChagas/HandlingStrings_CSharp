abc2e579aae163046dbf592beb1d37cf81547256
dyggo <dyggo@DESKTOP-NEOQB0Q>
2022-01-28 15:26:02 -0300

    Adicionar arquivos de projeto.

---
+++ projetoURL/ExtratorValorDeArgumentos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoURL
+{
+    internal class ExtratorValorDeArgumentos
+    {
+        public string URL { get; }
+        public string _argumentos { get; }
+
+        public ExtratorValorDeArgumentos(string url)
+        {
+            if (string.IsNullOrEmpty(url)) 
+            {
+                throw new ArgumentException("String nao pode ser nula ou estar vazia!!", nameof(url));
+            }
+            int indice = url.IndexOf('?');
+            _argumentos = url.Substring(indice + 1);
+
+            URL = url;
+        }
+
+        public string getValor(string parametro) 
+        {
+            parametro = parametro.ToUpper();
+            string argumentosUpper = _argumentos.ToUpper();  
+            
+            string termo = parametro + "=" ;
+            int indiceTermo = argumentosUpper.IndexOf(termo);
+            
+            string valor = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = valor.IndexOf('&');
+            
+            if (indiceEComercial == -1) 
+            {
+                return valor; 
+            }
+
+            return valor.Remove(indiceEComercial);
+        }
+
+    }
+}
+
---
+++ projetoURL/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoURL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestaManipulacaoDaURL();
+
+            Console.ReadLine();
+        
+        }
+            public static void TestaManipulacaoDaURL() {
+                string URL = "https://www.netshoes.com.br/busca?nsCat=Natural&q=vasco";
+
+                ExtratorValorDeArgumentos extratorValorDeArgumentos = new ExtratorValorDeArgumentos(URL);
+
+                string valor = extratorValorDeArgumentos.getValor("q");
+
+                Console.WriteLine(valor);
+            }
+    }
+}
